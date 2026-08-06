0e69af224fdacd68e5195ecc19e1ff428e1e355c
mohammodrubel <md.rubel007@hotmail.com>
2026-08-06 18:59:42 +0600

    update api key

--- redux/api/baseApi.tsx
+++ redux/api/baseApi.tsx
@@ -3,7 +3,7 @@ import { RootState } from "../store";
 import { tagTypesList } from "../TagTypes";
 
 const baseQuery = fetchBaseQuery({
-  baseUrl: "https://ecomarce-backend-0djv.onrender.com/api",
+  baseUrl: "https://ecomarce-backend-7oba.onrender.com/api",
   prepareHeaders: (headers, { getState }) => {
     const state = getState() as RootState;
     const token = state.auth?.token;
